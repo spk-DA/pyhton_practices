508191173ad1f679051297813048047013d8cb56
spk-DA <spk011251@gmail.com>
2026-09-16 10:21:36 +0530

    Create Loopbased.sql

---
+++ Loopbased.sql
@@ -0,0 +1,13 @@
+3. LOOP-BASED QUESTIONS
+Use for and/or while loops.
+# 3. LOOP-BASED QUESTIONS
+# Use for and/or while loops.
+
+# Print numbers from 1 to 10.
+# i=0
+# while (i<10):
+#   i=i+1
+#   print(i)
+
+# Print numbers from 10 to 1.
+…     print(a, "*" ,i, "=",d)
